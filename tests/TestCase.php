cc633e4a5d5677a0962e3cc52ed3fe888b7e4300
enricodelazzari <enrico.delazzari@h-farm.com>
2022-06-11 15:47:45 +0200

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,7 +3,6 @@
 namespace EnricoDeLazzari\HtmlTableToArray\Tests;
 
 use EnricoDeLazzari\HtmlTableToArray\HtmlTableToArrayServiceProvider;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
@@ -11,10 +10,6 @@ class TestCase extends Orchestra
     protected function setUp(): void
     {
         parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'EnricoDeLazzari\\HtmlTableToArray\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
     }
 
     protected function getPackageProviders($app)
@@ -26,11 +21,6 @@ class TestCase extends Orchestra
 
     public function getEnvironmentSetUp($app)
     {
-        config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_laravel-html-table-to-array_table.php.stub';
-        $migration->up();
-        */
+        // 
     }
 }
